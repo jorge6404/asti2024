17f3e0444e5ceccfc2e189afa3fef3f00fffeb66
diegomarzaa <al426641@uji.es>
2024-02-26 21:31:47 +0100

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Preguntar por el parametro
+echo -n "Por favor, introduzca un número (1,2,3,4): "
+read parametro
+
+# Ejecutar el comando dependiendo del parámetro
+case $parametro in
+  1) 
+    echo "$parametro"
+    # Inicia una nueva sesión de tmux en segundo plano
+    tmux new-session -d -s my_session 
+
+    # Divida la ventana de tmux en dos paneles
+    tmux split-window
+	
+    # Envia las teclas al primer panel
+    tmux send-keys -t my_session:0.0 'source /opt/ros/foxy/setup.bash' Enter
+    tmux send-keys -t my_session:0.0 'source install/setup.bash' Enter
+    tmux send-keys -t my_session:0.0 'ros2 run bringup motor_controller /dev/ttyUSB1' Enter
+	
+    # Envia las teclas al segundo panel
+    tmux send-keys -t my_session:0.1 'source /opt/ros/foxy/setup.bash' Enter
+    tmux send-keys -t my_session:0.1 'source install/setup.bash' Enter
+    tmux send-keys -t my_session:0.1 'ros2 topic pub -1 /set_velocity custom_interfaces/SetVelocity "{id: 1, velocity: 500}" ' Enter
+ 
+    # Adjunta la sesión
+    tmux attach -t my_session
+    ;;
+  2) 
+    echo "$parametro"
+    ;;
+  3) 
+    echo "$parametro"
+    ;;
+  4) 
+    echo "$parametro"
+    tmux send-keys -t my_session:0.1 'ros2 topic pub -1 /set_velocity custom_interfaces/SetVelocity "{id: 1, velocity: 0}" ' Enter
+ 
+    ;;
+  *) 
+    echo "Por favor, introduzca un número válido (1,2,3,4)"
+    ;;
+esac
