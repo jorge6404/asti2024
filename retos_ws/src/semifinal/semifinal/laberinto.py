25eca56466d09b7f9c4d58eb16fe904f422e2249
diegomarzaa <al426641@uji.es>
2024-02-29 19:55:15 +0100

    custom_interfaces_bien

--- retos_ws/src/semifinal/semifinal/laberinto.py
+++ retos_ws/src/semifinal/semifinal/laberinto.py
@@ -1,25 +1,24 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
-
+from custom_interfaces.msg import SetVelocity
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(SetVelocity, 'set_velocity', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello Woasdasrld: %d' % self.i
+        msg = SetVelocity()
+        msg.id = 1
+        msg.velocity = 5
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
-
+        self.get_logger().info('Publishing: "%s"' % msg.id)
+        self.get_logger().info('Publishing: "%s"' % msg.velocity)
 
 def main(args=None):
     rclpy.init(args=args)
