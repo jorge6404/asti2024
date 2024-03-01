1f1aa22ca1029a1c1e13642e42c76ea6331183c5
JosepMarinG <123630716+JosepMarinG@users.noreply.github.com>
2024-03-01 22:30:50 +0100

    rosbag_manual

--- retos_ws/src/semifinal/setup.py
+++ retos_ws/src/semifinal/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'laberinto = semifinal.laberinto:main'
+            'laberinto = semifinal.laberinto:main',
+            'rosBagManual = semifinal.rosBagManual:main'
         ],
     },
 )
