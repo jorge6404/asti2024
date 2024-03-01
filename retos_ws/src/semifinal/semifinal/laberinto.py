d033c0657660705a59f05fcc31bb3ac020583e2a
guillemfustier <al426661@uji.es>
2024-03-01 17:21:51 +0100

    funciona bien robot + funciones laberinto

--- retos_ws/src/semifinal/semifinal/laberinto.py
+++ retos_ws/src/semifinal/semifinal/laberinto.py
@@ -17,22 +17,65 @@ class MinimalPublisher(Node):
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing: id="{msg.id}", velocity="{msg.velocity}"')
 
+def recto(pub, vel, tiempo):
+    # Publish velocity to id 1 and 2
+    pub.publish_velocity(1, -vel)
+    pub.publish_velocity(2, vel)
+
+    # Wait for 'tiempo' seconds
+    time.sleep(tiempo)
+
+    # Publish velocity 0 to id 1 and 2
+    pub.publish_velocity(1, 0)
+    pub.publish_velocity(2, 0)
+
+def derecha(pub, vel, tiempo):
+    # Publish velocity to id 1 and 2
+    pub.publish_velocity(1, -vel)
+    pub.publish_velocity(2, -vel)
+
+    # Wait for 'tiempo' seconds
+    time.sleep(tiempo)
+
+    # Publish velocity 0 to id 1 and 2
+    pub.publish_velocity(1, 0)
+    pub.publish_velocity(2, 0)
+
+def izquierda(pub, vel, tiempo):
+    # Publish velocity to id 1 and 2
+    pub.publish_velocity(1, vel)
+    pub.publish_velocity(2, vel)
+
+    # Wait for 'tiempo' seconds
+    time.sleep(tiempo)
+
+    # Publish velocity 0 to id 1 and 2
+    pub.publish_velocity(1, 0)
+    pub.publish_velocity(2, 0)
+
+def atras(pub, vel, tiempo):
+    # Publish velocity to id 1 and 2
+    pub.publish_velocity(1, vel)
+    pub.publish_velocity(2, -vel)
+
+    # Wait for 'tiempo' seconds
+    time.sleep(tiempo)
+
+    # Publish velocity 0 to id 1 and 2
+    pub.publish_velocity(1, 0)
+    pub.publish_velocity(2, 0)
+
 def main(args=None):
     rclpy.init(args=args)
 
     minimal_publisher = MinimalPublisher()
     executor = SingleThreadedExecutor()
 
-    # Publish velocity to id 1 and 2
-    minimal_publisher.publish_velocity(1, 20)
-    minimal_publisher.publish_velocity(2, -20)
+    time.sleep(2)
+    recto(minimal_publisher, 200, 4)
+    derecha(minimal_publisher, 180, 2)
+    recto(minimal_publisher, 200, 4)
 
-    # Wait for 3 seconds
-    time.sleep(3)
-
-    # Publish velocity 0 to id 1 and 2
-    minimal_publisher.publish_velocity(1, 0)
-    minimal_publisher.publish_velocity(2, 0)
 
     executor.add_node(minimal_publisher)
     try:
