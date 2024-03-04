7cd29ac3588e02343073498b677d50a01f3f8317
Jcrex <al428669@uji.es>
2024-03-04 11:35:11 +0100

    Actualizacion del codigo detectar_linea, ahora esta en siguelinea guillem

--- retos_ws/src/semifinal/semifinal/siguelineas_guillem.py
+++ retos_ws/src/semifinal/semifinal/siguelineas_guillem.py
@@ -3,8 +3,10 @@ import numpy as np
 from time import sleep
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import Int32MultiArray
 from geometry_msgs.msg import Twist
 from semifinal.misfunciones import *
+
 
 class DetectLinea(Node):
     def __init__(self):
@@ -14,12 +16,19 @@ class DetectLinea(Node):
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(0)  # /home/jcrex/Vídeos/siguelineas_largo.mp4
         self.cap.set(3, 640)
         self.cap.set(4, 480)
+        self.divisiones = 7
 
     def timer_callback(self):
         self.detectar()
+
+    def publish(self, tupla):
+        msg = Int32MultiArray()
+        msg.data = tupla
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
 
     def publish_velocity(self, velocity):            # velocity -->  tuple = (1, 0) (linear.x, angular.z)
         msg = Twist()
@@ -28,76 +37,134 @@ class DetectLinea(Node):
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing: velocity="({msg.linear.x}, {msg.angular.z})"')
 
+    def camara(self):
+        # Uso para el testeo de camara
+        while True:
+            success, img = self.cap.read()
+            cv2.imshow("Video", img)
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+
+    def detectar_puntos_activos(self, cuadricula, hImg, wImg, img, puntos):
+        for i in range(self.divisiones):
+            for j in range(self.divisiones):
+                if np.any(cuadricula[i][j] > 0):
+                    # print(((j * 2 + 1) * wImg // 14, (i * 2 + 1) * hImg // 14))
+                    cv2.circle(img, ((j * 2 + 1) * wImg // (2*self.divisiones), (i * 2 + 1) * hImg // (2*self.divisiones)),
+                               5, (50, 50, 255), 2)
+                    if (i > (self.divisiones - 3) and j < 2) or (i < 2 and j < 2):
+                        puntos[i][j] = 2
+                    elif (i > (self.divisiones - 3) and j > (self.divisiones - 3)) or (i < 2 and j > (self.divisiones - 3)):
+                        puntos[i][j] = 2
+                    else:
+                        puntos[i][j] = 1
+
+    def calcular_diferencia_puntos(self, puntos):
+        suma_columna_central = sum(puntos[i][self.divisiones // 2] for i in range(self.divisiones))
+        suma_derecha = sum(sum(puntos[i][self.divisiones // 2 + 1:]) for i in range(self.divisiones))
+        suma_izquierda = sum(sum(puntos[i][:self.divisiones // 2 - 1]) for i in range(self.divisiones))
+        diferencia_posiciones = suma_derecha - suma_izquierda
+        print(suma_derecha, suma_izquierda, diferencia_posiciones)
+        return suma_columna_central, diferencia_posiciones
+
+    def movimiento(self, vel):
+        if vel < -self.divisiones + (self.divisiones // 2):
+            print("Girar izquierda")
+            self.publish_velocity((0.0, -0.1))
+
+        elif vel > self.divisiones // 0.9:
+            print("Girar derecha")
+            self.publish_velocity((0.0, 0.1))
+        else:
+            print("ir recto")
+            self.publish_velocity((0.1, 0.0))
+
     def detectar(self):
         while True:
             success, img = self.cap.read()
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            cv2.circle(img, (320, 480), 5, (50, 50, 255), 2)
             hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
             lower_blue = np.array([0, 0, 0])
-            upper_blue = np.array([80, 80, 80])
+            upper_blue = np.array([50, 50, 50])
 
             mask = cv2.inRange(hsv, lower_blue, upper_blue)
             resultado = cv2.bitwise_and(img, img, mask=mask)
 
             hImg, wImg, _ = resultado.shape
 
-            cuadricula49 = [
-                [resultado[i * hImg // 7:(i + 1) * hImg // 7, j * wImg // 7:(j + 1) * wImg // 7] for j in range(7)]
-                for i in range(7)
+            cuadricula = [
+                [resultado[i * hImg // self.divisiones:(i + 1) * hImg // self.divisiones,
+                 j * wImg // self.divisiones:(j + 1) * wImg // self.divisiones] for j in range(self.divisiones)]
+                for i in range(self.divisiones)
             ]
 
-            puntos = [[0]*7 for _ in range(7)]
-            self.detectar_puntos_activos(resultado, hImg, wImg, img, puntos)
-            
-            diferencia = self.calcular_diferencia_puntos(puntos)
+            puntos = [[0] * self.divisiones for _ in range(self.divisiones)]
+            self.detectar_puntos_activos(cuadricula, hImg, wImg, img, puntos)
 
-            #self.publish((suma_columna_central, diferencia_posiciones))
-            ####
-            # cv2.imshow("Result", resultado)
+            suma_central, diferencia = self.calcular_diferencia_puntos(puntos)
+            #print(suma_central, diferencia, suma_central + diferencia, self.divisiones + diferencia)
+
+            self.movimiento(diferencia)
+
+            #self.publish((suma_central, diferencia))
+
+            cv2.imshow("Result", resultado)
             cv2.imshow("Video", img)
-            ###
 
-            """
-            ESTO CORRESPONDE AL MAIN():
-            """
-            if diferencia < -5:
-                print("girar izquierda")
-                girar_izquierda(self)
-
-            elif diferencia > 5:
-                print("girar derecha")
-                girar_derecha(self)
-
-            else:
-                print("avanzar recto")
-                avanzar(self)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(100) & 0xFF == ord('q'):
                 break
 
-    def detectar_puntos_activos(self, resultado, hImg, wImg, img, puntos):
-        for i in range(7):
-            for j in range(7):
-                if np.any(resultado[i * hImg // 7:(i + 1) * hImg // 7, j * wImg // 7:(j + 1) * wImg // 7] > 0):
-                    cv2.circle(img, ((j * 2 + 1) * wImg // 14, (i * 2 + 1) * hImg // 14), 5, (50, 50, 255), 2)
-                    puntos[i][j] = 1
-                    
-    
-    def calcular_diferencia_puntos(self, puntos):
-        suma_derecha = sum(sum(puntos[i][4:]) for i in range(7))
-        suma_izquierda = sum(sum(puntos[i][:3]) for i in range(7))
-        diferencia_posiciones = suma_derecha - suma_izquierda
-        print(suma_izquierda, suma_derecha, diferencia_posiciones)
-        return diferencia_posiciones
+
+def menu():
+
+    opcion = int(input(
+        "Menu de control del siguelineas elegir: \n"
+        "1 para empezar a detectar linea y moverse \n"
+        "2 para controlar los movimiento de forma manual \n"
+        "3 Mostrar la camara y luego ejecutar codigo \n"
+    ))
+    print(f"Opcion seleccionada {opcion}")
+    return opcion
 
 
 def main(args=None):
-    rclpy.init(args=args)
-    detectar_linea = DetectLinea()
-    detectar_linea.detectar()
-    rclpy.spin(detectar_linea)
-    detectar_linea.destroy_node()
+    opcion = menu()
+    if opcion == 1:
+        rclpy.init(args=args)
+        detectar_linea = DetectLinea()
+        rclpy.spin(detectar_linea)
+        detectar_linea.destroy_node()
+
+    elif opcion == 2:
+        rclpy.init(args=args)
+        movimiento = DetectLinea()
+        opcion = input(
+            "Elija la direccion que tomara: \n"
+            "Valor < -3 girar a la izquierda \n"
+            "Valor > 3 girar a la derecha \n"
+            "Valor intermedio ir recto \n"
+            "No poner nada acaba el programa \n"
+        )
+        while opcion != "":
+            movimiento.movimiento(int(opcion))
+            opcion = input(
+                "Elija la direccion que tomara: \n"
+                "Valor < -3 girar a la izquierda \n"
+                "Valor > 3 girar a la derecha \n"
+                "Valor intermedio ir recto \n"
+                "No poner nada acaba el programa \n"
+            )
+        #rclpy.spin(movimiento)
+        movimiento.destroy_node()
+
+    elif opcion == 3:
+        rclpy.init(args=args)
+        camara = DetectLinea()
+        print("activada")
+        camara.camara()
+        rclpy.spin(camara)
+        camara.destroy_node()
+
     rclpy.shutdown()
 
 
