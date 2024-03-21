283e08621647dd43bbd9b0dada1fb8ad11bf561b
Jcrex <al428669@uji.es>
2024-03-21 17:55:35 +0100

    mejora de los launchs

--- retos_ws/src/simulador_gazebo/launch/launch_sim_cuadricula.launch.py
+++ retos_ws/src/simulador_gazebo/launch/launch_sim_cuadricula.launch.py
@@ -42,12 +42,15 @@ def generate_launch_description():
         )
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    world = input("Ingrese el mundo a cargar: ")
+    world_file_path = os.path.join(get_package_share_directory(package_name), 'worlds', world)
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file,
+                                      'world': world_file_path }.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
