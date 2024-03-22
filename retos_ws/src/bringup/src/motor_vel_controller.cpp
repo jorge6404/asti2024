2e64a3393619980301bb1273599239e6a612a7e6
diegomarzaa <al426641@uji.es>
2024-03-22 21:08:36 +0100

    motor_vel_con_herramienta

--- retos_ws/src/bringup/src/motor_vel_controller.cpp
+++ retos_ws/src/bringup/src/motor_vel_controller.cpp
@@ -27,8 +27,11 @@
 #include "rclcpp/rclcpp.hpp"
 #include "dynamixel_sdk/dynamixel_sdk.h"
 #include "custom_interfaces/srv/get_velocity.hpp"
+#include "custom_interfaces/srv/get_position.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "rcutils/cmdline_parser.h" // Dynamixel SDK
+#include "std_msgs/msg/float32.hpp"
+
 
 #include "motor_vel_controller.hpp"
 
@@ -37,6 +40,8 @@
 #define ADDR_TORQUE_ENABLE 64 // 0 for torque off | 1 for torque on
 #define ADDR_GOAL_VELOCITY 104
 #define ADDR_PRESENT_VELOCITY 128
+#define ADDR_GOAL_POSITION 116
+#define ADDR_PRESENT_POSITION 132
 
 // Protocol version
 #define PROTOCOL_VERSION 2.0
@@ -53,7 +58,9 @@
 dynamixel::PortHandler *portHandler;
 dynamixel::PacketHandler *packetHandler;
 
+uint8_t id_herramienta = 3;
 uint8_t dxl_error = 0;
+uint32_t goal_position = 0;
 uint32_t right_wheel_velocity = 0;
 uint32_t left_wheel_velocity = 0;
 int dxl_comm_result = COMM_TX_FAIL;
@@ -133,7 +140,39 @@ MotorController::MotorController()
          } else if (dxl_error != 0) {
             RCLCPP_ERROR(this->get_logger(), "%s", packetHandler->getRxPacketError(dxl_error));
          } else {
-            RCLCPP_INFO(this->get_logger(), "Set [ID: %d] [Goal Velocity: %d]", 1, left_wheel_velocity);
+            RCLCPP_INFO(this->get_logger(), "Set [ID: %d] [Goal Velocity: %d]", 2, left_wheel_velocity);
+         }
+      }
+   );
+
+   tool_pos_subscriber_ =
+      this->create_subscription<SetPosition>(
+      "tool_pos",
+      QOS_RKL10V,
+      [this](const SetPosition::SharedPtr msg) -> void {
+         uint8_t dxl_error = 0;
+
+         // Read linear velocity
+         uint32_t goal_position = (unsigned int) msg->position;   // Convert int32 -> uint32
+         
+         uint32_t calculo = goal_position * 11.375;
+
+         // Write goal position (4 bytes) to the DYNAMIXEL
+         dxl_comm_result =
+         packetHandler->write4ByteTxRx(
+            portHandler, 
+            id_herramienta, // Tool ID        // (uint8_t) msg->id,
+            ADDR_GOAL_POSITION,
+            calculo,
+            &dxl_error
+         );
+
+         if (dxl_comm_result != COMM_SUCCESS) {
+            RCLCPP_ERROR(this->get_logger(), "%s", packetHandler->getTxRxResult(dxl_comm_result));
+         } else if (dxl_error != 0) {
+            RCLCPP_ERROR(this->get_logger(), "%s", packetHandler->getRxPacketError(dxl_error));
+         } else {
+            RCLCPP_INFO(this->get_logger(), "Set [ID: %d] [Goal Position: %d]", 3, calculo);
          }
       }
    );
@@ -163,6 +202,34 @@ MotorController::MotorController()
          response->velocity = current_velocity;
       };
    get_velocity_server_ = create_service<GetVelocity>("get_velocity", get_current_velocity);
+
+   // Defines a service to get the motor's current position
+   auto get_present_position =
+    [this](
+    const std::shared_ptr<GetPosition::Request> request,
+    std::shared_ptr<GetPosition::Response> response) -> void
+    {
+      // Read Present Position (length : 4 bytes) and Convert uint32 -> int32
+      // When reading 2 byte data from AX / MX(1.0), use read2ByteTxRx() instead.
+      dxl_comm_result = packetHandler->read4ByteTxRx(
+        portHandler,
+        (uint8_t) request->id,
+        ADDR_PRESENT_POSITION,
+        reinterpret_cast<uint32_t *>(&present_position),
+        &dxl_error
+      );
+
+      RCLCPP_INFO(
+        this->get_logger(),
+        "Get [ID: %d] [Present Position: %d]",
+        request->id,
+        present_position
+      );
+
+      response->position = present_position;
+    };
+
+  get_position_server_ = create_service<GetPosition>("get_position", get_present_position);
 }
 
 MotorController::~MotorController() { RCLCPP_INFO(this->get_logger(), "Motor Controller node stopped"); }
@@ -183,6 +250,14 @@ void setupDynamixel(uint8_t dxl_id) {
    } else {
       RCLCPP_INFO(rclcpp::get_logger("motor_controller"), "Succeeded to set Velocity Control mode.");
    }
+
+   dxl_comm_result = packetHandler->write1ByteTxRx(
+      portHandler, 
+      id_herramienta,
+      ADDR_OPERATING_MODE, 
+      3,
+      &dxl_error
+   );
 
    // Enable Torque so the motor can move (EEPROM will be locked)
    dxl_comm_result = packetHandler->write1ByteTxRx(
